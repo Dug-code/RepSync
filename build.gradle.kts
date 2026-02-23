4290fadf67e2a1fec25e8bb40e395aa56546d04b
Dug-code <douglas.bluemel@gmail.com>
2026-02-23 13:43:14 -0500

    icon change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    // It's generally safer to use slightly older, more stable versions
+    id("com.android.application") version "8.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false // A recent, stable version
 }
+
