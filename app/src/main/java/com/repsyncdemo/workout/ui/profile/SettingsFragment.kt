dff001b8e0e17066c054933598f74f283f894a76
aguirre21 <rodaguirre56@gmail.com>
2026-04-20 01:08:29 -0400

    Cleaned imports

--- app/src/main/java/com/repsyncdemo/workout/ui/profile/SettingsFragment.kt
+++ app/src/main/java/com/repsyncdemo/workout/ui/profile/SettingsFragment.kt
@@ -1,6 +1,5 @@
 package com.repsyncdemo.workout.ui.profile
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
@@ -12,14 +11,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import coil.load
 import coil.transform.CircleCropTransformation
-import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.firebase.auth.FirebaseAuth
